9ace062304167b77c595e413571396a9d11485b1
IwanPWK <koadhyow@gmail.com>
2023-12-26 23:22:04 +0700

    Get lang & lng drop-off location logic

--- lib/widgets/place_prediction_tile.dart
+++ lib/widgets/place_prediction_tile.dart
@@ -1,15 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../assistants/request_assistant.dart';
+import '../globals/map_key.dart';
+import '../info_handler/app_info.dart';
+import '../models/directions.dart';
 import '../models/predicted_places.dart';
+import 'progress_dialog.dart';
 
 class PlacePredictionTileDesign extends StatelessWidget {
   final PredictedPlaces? predictedPlaces;
 
   const PlacePredictionTileDesign({super.key, this.predictedPlaces});
 
+  getPlaceDirectionDetails(String? placeId, context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => const ProgressDialog(
+        message: "Setting Up Drof-Off, Please wait...",
+      ),
+    );
+
+    String placeDirectionDetailsUrl =
+        "https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$mapKey";
+
+    var responseApi =
+        await RequestAssistant.receiveRequest(placeDirectionDetailsUrl);
+
+    Navigator.pop(context);
+
+    if (responseApi == "Error Occurred, Failed. No Response.") {
+      return;
+    }
+
+    if (responseApi["status"] == "OK") {
+      Directions directions = Directions();
+      directions.locationName = responseApi["result"]["name"];
+      directions.locationId = placeId;
+      directions.locationLatitude =
+          responseApi["result"]["geometry"]["location"]["lat"];
+      directions.locationLongitude =
+          responseApi["result"]["geometry"]["location"]["lng"];
+
+      print('Location Name = ${directions.locationName!}');
+      print('Location Id = ${directions.locationId!}');
+      print('Location Latitude = ${directions.locationLatitude.toString()}');
+      print('Location Longitude = ${directions.locationLongitude.toString()}');
+
+      // Provider.of<AppInfo>(context, listen: false)
+      //     .updateDropOffLocationAddress(directions);
+
+      Navigator.pop(context, "obtainedDropoff");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        getPlaceDirectionDetails(predictedPlaces!.place_id, context);
+      },
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.white24,
       ),
